a9f75ed6421df9553da30d1d3ec37fd9ad48586f
Niki <nsobadzhiev@broadsoft.com>
2014-09-16 18:44:26 +0300

    Added the project and DMDynamicPageViewController files

---
+++ DynamicPageViewController/AppDelegate.swift
@@ -0,0 +1,93 @@
+//  The MIT License (MIT)
+//
+//  Copyright (c) 2014 Nikola Sobadjiev
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+                            
+    var window: UIWindow?
+    var pageController:DMDynamicViewController? = nil
+
+    func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
+        // Override point for customization after application launch.
+        let viewController1 = UIViewController()
+        let viewController2 = UIViewController()
+        let viewController3 = UIViewController()
+        let viewController4 = UIViewController()
+        let viewController5 = UIViewController()
+        
+        viewController1.view.backgroundColor = UIColor.redColor()
+        viewController2.view.backgroundColor = UIColor.greenColor()
+        viewController3.view.backgroundColor = UIColor.blueColor()
+        viewController4.view.backgroundColor = UIColor.blackColor()
+        viewController5.view.backgroundColor = UIColor.yellowColor()
+        
+        let viewControllers = [viewController1, viewController2, viewController3, viewController4, viewController5]
+        pageController = DMDynamicViewController(viewControllers: viewControllers)
+        window = UIWindow()
+        window?.frame = UIScreen.mainScreen().bounds
+        pageController?.view.frame = window!.frame;
+        window?.addSubview(pageController!.view)
+        window?.rootViewController = pageController
+        window?.makeKeyAndVisible()
+        
+//        var timerRemove = NSTimer.scheduledTimerWithTimeInterval(5.1, target: self, selector: Selector("removeController"), userInfo: nil, repeats: false)
+//        var timerAdd = NSTimer.scheduledTimerWithTimeInterval(10.1, target: self, selector: Selector("addController"), userInfo: nil, repeats: false)
+        return true
+    }
+    
+    func removeController() {
+        self.pageController?.removePage(self.pageController!.viewControllers![0])
+    }
+    
+    func addController() {
+        let newController = UIViewController()
+        newController.view.backgroundColor = UIColor.purpleColor()
+        self.pageController?.insertPage(newController, atIndex: 0)
+    }
+
+    func applicationWillResignActive(application: UIApplication!) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(application: UIApplication!) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(application: UIApplication!) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(application: UIApplication!) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(application: UIApplication!) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+}
+
---
+++ DynamicPageViewController/DMDynamicViewController.swift
@@ -0,0 +1,187 @@
+//  The MIT License (MIT)
+//
+//  Copyright (c) 2014 Nikola Sobadjiev
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+import UIKit
+
+protocol DMDynamicPageViewControllerDelegate {
+    func pageViewController(pageController: DMDynamicViewController, didSwitchToViewController viewController: UIViewController)
+    func pageViewController(pageController: DMDynamicViewController, didChangeViewControllers viewControllers: Array<UIViewController>)
+}
+
+class DMDynamicViewController: UIViewController, UIScrollViewDelegate {
+    
+    var containerScrollView: UIScrollView! = nil
+    var pageWidth: CGFloat = 1.0
+    var viewControllers:Array<UIViewController>? = nil {
+        didSet {
+            self.layoutPages()
+        }
+    }
+    var currentPage:Int = 0 {
+        didSet {
+            if (currentPage >= self.viewControllers?.count)
+            {
+                self.currentPage = self.viewControllers!.count - 1
+            }
+            
+            containerScrollView.delegate = nil
+            containerScrollView.contentOffset = CGPointMake(CGFloat(self.currentPage) * self.view.bounds.size.width, 0.0)
+            containerScrollView.delegate = self
+            // Set the fully switched page in order to notify the delegates about it if needed.
+            self.fullySwitchedPage = self.currentPage;
+        }
+    }
+    var fullySwitchedPage:Int = 0 {
+        didSet {
+            if (oldValue != self.fullySwitchedPage) {
+                // The page is fully switched.
+                if (self.fullySwitchedPage < self.viewControllers?.count) {
+                    let previousViewController = self.viewControllers?[self.fullySwitchedPage];
+                    // Perform the "disappear" sequence of methods manually when the view of
+                    // the controller is not visible at all.
+                    previousViewController?.willMoveToParentViewController(self)
+                    previousViewController?.viewWillDisappear(false)
+                    previousViewController?.viewDidDisappear(false)
+                    previousViewController?.didMoveToParentViewController(self)
+                    previousViewController?.willMoveToParentViewController(self)
+                    previousViewController?.viewWillAppear(false)
+                    previousViewController?.viewDidAppear(false)
+                    previousViewController?.didMoveToParentViewController(self)
+                }
+            }
+        }
+    }
+    var delegate: DMDynamicPageViewControllerDelegate? = nil
+                            
+    init(viewControllers: Array<UIViewController>) {
+        super.init()
+        self.viewControllers = viewControllers
+        self.notifyDelegateDidChangeControllers()
+    }
+    
+    required override init(nibName nibNameOrNil: String!, bundle nibBundleOrNil: NSBundle!) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func willMoveToParentViewController() {
+        let page = Int((containerScrollView.contentOffset.x - pageWidth / 2.0) / pageWidth) + 1
+        if (self.currentPage != page)
+        {
+            currentPage = page;
+            fullySwitchedPage = page;
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        for var i = 0; i < self.viewControllers?.count; i += 1 {
+            let pageX:CGFloat = CGFloat(i) * self.view.bounds.size.width
+            self.viewControllers?[i].view.frame = CGRectMake(pageX, 0.0, self.view.bounds.size.width, self.view.bounds.size.height)
+        }
+        // It is important to set the pageWidth property before the contentSize and contentOffset,
+        // in order to use the new width into scrollView delegate methods.
+        pageWidth = self.view.bounds.size.width
+        containerScrollView.contentSize = CGSizeMake(CGFloat(self.viewControllers!.count) * self.view.bounds.size.width, 1.0)
+        containerScrollView.contentOffset = CGPointMake(CGFloat(self.currentPage) * self.view.bounds.size.width, 0.0)
+    }
+    
+    func layoutPages() {
+        for pageView in containerScrollView.subviews {
+            pageView.removeFromSuperview()
+        }
+        
+        for var i = 0; i < self.viewControllers?.count; i++ {
+            let page = self.viewControllers?[i]
+            self.addChildViewController(page)
+            let nextFrame:CGRect = CGRectMake(CGFloat(i) * self.view.bounds.size.width, self.view.frame.origin.y, self.view.frame.size.width, self.view.frame.size.height)
+            page?.view.frame = nextFrame
+            containerScrollView.addSubview(page!.view)
+            page?.didMoveToParentViewController(self)
+        }
+        containerScrollView.contentSize = CGSizeMake(self.view.bounds.size.width * CGFloat(self.viewControllers!.count), 1.0)
+    }
+    
+    func insertPage(viewController: UIViewController, atIndex index: Int) {
+        self.viewControllers?.insert(viewController, atIndex: index)
+        self.layoutPages()
+        self.currentPage = index
+        self.notifyDelegateDidChangeControllers()
+    }
+    
+    func removePage(viewController: UIViewController) {
+        for var i = 0; i < self.viewControllers?.count; i += 1 {
+            if (self.viewControllers?[i] == viewController) {
+                self.viewControllers?.removeAtIndex(i)
+                self.layoutPages()
+                self.notifyDelegateDidChangeControllers()
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        containerScrollView = UIScrollView(frame: self.view.bounds)
+        containerScrollView.autoresizingMask = UIViewAutoresizing.FlexibleHeight | UIViewAutoresizing.FlexibleWidth
+        containerScrollView.pagingEnabled = true
+        containerScrollView.alwaysBounceVertical = false
+        containerScrollView.showsHorizontalScrollIndicator = false
+        containerScrollView.delegate = self
+        containerScrollView.backgroundColor = UIColor.grayColor()
+        self.pageWidth = self.view.frame.size.width
+        self.view.addSubview(containerScrollView)
+        self.layoutPages()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func notifyDelegateDidSwitchPage() {
+        self.delegate?.pageViewController(self, didSwitchToViewController: self.viewControllers![self.currentPage])
+    }
+    
+    func notifyDelegateDidChangeControllers() {
+        self.delegate?.pageViewController(self, didChangeViewControllers: self.viewControllers!)
+    }
+    
+    // MARK: UIScrollViewDelegate
+    
+    func scrollViewDidScroll(scrollView: UIScrollView!) {
+        // Update the page when more than 50% of the previous/next page is visible
+        let page = floor((containerScrollView.contentOffset.x - pageWidth / 2) / pageWidth) + 1
+        if (self.currentPage != Int(page)) {
+            // Check the page to avoid "index out of bounds" exception.
+            if (page >= 0 && Int(page) < self.viewControllers?.count) {
+                self.notifyDelegateDidSwitchPage()
+            }
+        }
+        // Check whether the current view controller is fully presented.
+        if (Int(containerScrollView.contentOffset.x) % Int(self.pageWidth) == 0)
+        {
+            self.fullySwitchedPage = self.currentPage;
+        }
+    }
+}
